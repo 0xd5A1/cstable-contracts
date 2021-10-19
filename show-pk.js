42430386e642b63a07725a553b9a8a6db0c9b76d
0x3dA1 <0x5dA1@gmail.com>
2021-10-19 10:06:25 +0800

    fill mnemonics improve shwo pk

--- show-pk.js
+++ show-pk.js
@@ -3,10 +3,12 @@
 const ethers = require('ethers');
 const mnemonics = require('./secret.js');
 
+let whoes = process.argv[2];
+
 // 初始化rpc provider，浏览器中不需要
 // const provider = new ethers.providers.JsonRpcProvider(config.default.rpc.url);
 // 初始化助记词
-const walletMnemonic = ethers.Wallet.fromMnemonic(mnemonics.deployer);
+const walletMnemonic = ethers.Wallet.fromMnemonic(mnemonics[whoes]);
 // 初始化钱包
 // const wallet = walletMnemonic.connect(provider);
 console.log(walletMnemonic.address);
